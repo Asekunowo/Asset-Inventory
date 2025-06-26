ffdeac3a727d03e56fd416dbbac2e1a97911b5b5
ThisDevIsTired <michaelamao442@gmail.com>
2025-06-26 12:38:06 +0100

    remove logger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,22 +2,17 @@ import {Request, Response} from "express";
 import createApp from "./app";
 import {dbConn} from "./config/dbconfig";
 import {PORT} from "./secrets";
-import logger from "./utils/logger";
 
 const app = createApp();
 
 app.get("/", (req: Request, res: Response) => {
-	logger.info("GET / route accessed"); // Log an info message
-
 	res.send("Welcome to IT ASSET INVENTORY SERVER");
-	logger.verbose("Get requested finished executing");
 });
 
 app.listen(PORT, async () => {
 	const connected = await dbConn();
 	if (connected) {
 		console.log("Server running on port " + PORT);
-		logger.info(`Server listening on port ${PORT}`); // Log server start
 	} else {
 		throw new Error("Could not connect to db server");
 	}
